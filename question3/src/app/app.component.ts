11183200c73b44a7e76e1941bc67c12eb00439cb
Adam M. Seers <2128368@cegepmontpetit.ca>
2025-10-06 11:26:48 -0400

    PROGREsS erreurs

--- question3/src/app/app.component.ts
+++ question3/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup, ValidatorFn } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -17,12 +17,48 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 })
 export class AppComponent {
   title = 'reactive.form';
-
-  
-
-  constructor(
-    
-  ) { }
+  formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder){
+    this.formGroup = this.formBuilder.group(
+      {
+        nom: ['', [Validators.required]],
+        rue: ['', [Validators.required, Validators.min(1000), Validators.max(9999)]],
+        codePostal: ['', [Validators.pattern('^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$')]],
+        commentaire: ['', [Validators.required, est10mots()]],
+        formulaire: ['', [Validators.required, estPasNom()]],
+      },
+    );
+  }
 }
 
+export function est10mots(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const courriel = control.value;
+    // On regarde si le champ est rempli avant de faire la validation
+    if (!courriel) {
+      // On attend que le champ soit rempli avant de le valider
+      return null;
+    }
+    // On fait notre validation. Includes retourne un booléen.
+    const estValide = courriel.split(" ").length < 10;
+    // On retourne null si c'est valide, ou un objet décrivant l'erreur sinon
+    return estValide ? null : { est10mots: true };
+  };
+}
 
+export function estPasNom(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const courriel = control.value;
+    const nom = control.get('nom');
+    // On regarde si le champ est rempli avant de faire la validation
+    if (!courriel || !nom) {
+      // On attend que le champ soit rempli avant de le valider
+      return null;
+    }
+    // On fait notre validation. Includes retourne un booléen.
+    const estValide = courriel.value.includes(nom.value);
+
+    // On retourne null si c'est valide, ou un objet décrivant l'erreur sinon
+    return estValide ? null : { estPasNom: true };
+  };
+}
